f6e36f5ad643ab67ad45f3b4902a9b75778a49e6
DianaIvanovna <GDI.1999@yandex.ru>
2021-03-15 22:09:26 +0300

    feat: slider autopark

--- src/index.js
+++ src/index.js
@@ -1,38 +1,21 @@
 import "./style.scss";
 import "./index.html";
-// import Swiper JS
-import Swiper from 'swiper';
 
-// Import Swiper and modules
-import SwiperCore, { Navigation, Pagination, Scrollbar } from 'swiper/core';
+import Swiper from 'swiper'; // import Swiper JS
+import SwiperCore, { Navigation, Pagination, Scrollbar } from 'swiper/core'; // Import Swiper and modules
+SwiperCore.use([Navigation, Pagination, Scrollbar]); // Install modules
 
-// Install modules
-SwiperCore.use([Navigation, Pagination, Scrollbar]);
-
-const swiper = new Swiper('.swiper-container_my', {
+const swiperAutopark = new Swiper('.swiper-container_autopark', {
   // стрелки
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    // фракции
-    // type: 'fraction',
-    // renderFraction: function (currentClass, totalClass) {
-    //   return 'Фото <span class="' + currentClass + '"></span>' + ' из ' + '<span class="' + totalClass + '"></span>';
-    // }
-    // прогрессбар
-    type: "progressbar",
-
+    nextEl: '.swiper-button-next_autopark',
+    prevEl: '.swiper-button-prev_autopark',
   },
   // количество слайдов
-  slidesPerView: 2,
-  //Отключ функционал, если слайдов меньше
-  // watchOverflow: true,
+  slidesPerView: 3,
 
   // размер отступа
-  spaceBetween: 30,
+  spaceBetween: 25,
 
   //бесконечность
   loop: true,
@@ -48,6 +31,46 @@ const swiper = new Swiper('.swiper-container_my', {
   // }
 
 });
+
+// const swiper = new Swiper('.swiper-container_my', {
+//   // стрелки
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     // фракции
+//     // type: 'fraction',
+//     // renderFraction: function (currentClass, totalClass) {
+//     //   return 'Фото <span class="' + currentClass + '"></span>' + ' из ' + '<span class="' + totalClass + '"></span>';
+//     // }
+//     // прогрессбар
+//     type: "progressbar",
+
+//   },
+//   // количество слайдов
+//   slidesPerView: 2,
+//   //Отключ функционал, если слайдов меньше
+//   // watchOverflow: true,
+
+//   // размер отступа
+//   spaceBetween: 30,
+
+//   //бесконечность
+//   loop: true,
+
+//   // адаптивность
+//   // breakpoints: {
+//   //   320: {
+//   //     slidesPerView:1,
+//   //   },
+//   //   1040: {
+//   //     slidesPerView: 3
+//   //   }
+//   // }
+
+// });
 
 import Header from './js/Header';
 
